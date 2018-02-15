621c7606e07d21348854665a79d451e01930520f
Axelsson <elin.axelsson@pcm-gmi-51.gmi.oeaw.ac.at>
2018-02-15 11:42:25 +0100

    Fixed whitespace problem

--- rename.sh
+++ rename.sh
@@ -3,6 +3,10 @@
 pwd=$(pwd)
 pre=$(basename $pwd)
 
+## remove space
+find . -type f -name "*.tab" -exec bash -c 'mv "$0" "${0// /_}"' {} \;
+
+
 files=$(find . -name "*.tab")
 for f in $files; do
     bf=$(basename $f)
